c6bdf131a2fc8eebab87d05a6d4bde980c78edc2
@Laurine2002 <dong.laurine@gmail.com>
2020-09-24 19:00:25 +0100

    initialisation du projet

---
+++ partie 1/exo 3/google.js
@@ -0,0 +1,13 @@
+function color
+{
+    var MyArray= ("G","O","O","G","L","E");
+
+    MyArray[0]= G;
+    MyArray[1]= O;
+    MyArray[2]= O;
+    MyArray[3]= G;
+    MyArray[4]= L;
+    MyArray[5]= E;
+    A=parseInt(MyArray);
+    alert(a);
+}
